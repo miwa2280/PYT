586bba52cfdf3d07ac1be72ec12d9945ddcaa8c7
Mixa <houseyoupol@gmail.com>
2026-09-10 20:45:36 +0300

    tests

--- app/project/tests/test_utils.py
+++ app/project/tests/test_utils.py
@@ -1,5 +1,38 @@
+import pytest
+
 from utils import is_number_positive, concatenate_two_strings
 
+class TestIsNumberPositive:
+
+    @pytest.mark.parametrize(
+        'number, expected',
+        [
+            (-5, False),
+            (-5, False),
+            (-5, False),
+            (15, True),
+            (-5, False),
+            (0.1, True),
+            (-5, False),
+        ]
+    )
+    def test_is_number_positive_general(self, number, expected):
+        actual = is_number_positive(number)
+        assert expected is actual
+
+    @pytest.mark.skip(reason='Something strange here fix ASAP')
+    def test_is_number_positive_1(self):
+        1 / 0
+        number = 5
+        expected = True
+        actual = is_number_positive(number)
+        assert expected is actual
+
+    def test_is_number_positive_2(self):
+        number = -5
+        expected = False
+        actual = is_number_positive(number)
+        assert expected is actual
 
 def test_is_number_positive_1():
     number = 5
@@ -15,6 +48,12 @@ def test_is_number_positive_2():
     assert expected is actual
 
 
+def test_is_number_positive_2_1():
+    number = 0.01
+    expected = True
+    actual = is_number_positive(number)
+    assert expected is actual
+
 def test_is_number_positive_3():
     number = -5
     expected = False
@@ -25,6 +64,6 @@ def test_is_number_positive_3():
 def test_concatenate_two_strings_1():
     string_1 = '123'
     string_2 = '123'
-    expected = "1231223"
+    expected = "123123"
     actual = concatenate_two_strings(string_1, string_2)
     assert actual == expected, "what happened?"
