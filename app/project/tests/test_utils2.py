1cd012ef300805df498a584d2f69e670b0357df4
Mixa <houseyoupol@gmail.com>
2026-09-20 12:33:51 +0300

    tests

--- app/project/tests/test_utils2.py
+++ app/project/tests/test_utils2.py
@@ -1,15 +1,28 @@
-from utils import is_number_positive
+import pytest
+from utils import is_password_strong
 
 
-def test_is_number_positive_1():
-    number = 5
-    expected = True
-    actual = is_number_positive(number)
-    assert expected is actual
+class TestPasswordCheck:
+    @pytest.mark.parametrize(
+        'password, expected',
+        [
+            ('Password123!', True),     # Надежный
+            ('Secret#9', True),         # 8 символов
+            ('Qwer123!', True),         # 8 символов
+            ('Пароль123!', True),       # Кириллица
+            ('Short1!', False),         # Меньше 8 символов
+            ('Password123', False),     # Без спецсимвола
+            ('Password!!!!', False),    # Без цифр
+            ('123456789!#', False),     # Без букв
+            ('Pass word1!', False),     # Есть пробел
+            ('        ', False),        # Только пробелы
+            ('', False),                # Пустая строка
+        ]
+    )
+    def test_is_password_strong_parametrized(self, password: str, expected: bool):
+        actual = is_password_strong(password)
+        assert actual == expected
 
-
-def test_is_number_positive_2():
-    number = -5
-    expected = False
-    actual = is_number_positive(number)
-    assert expected is actual
+    @pytest.mark.skip(reason='Test is not ready yet')
+    def test_password_character_diversity(self):
+        pass
